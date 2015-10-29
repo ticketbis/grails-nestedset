3f7faaadb0a81a91a47348b811a0352bb0605106
jgargallo <jgargallo@gmail.com>
2015-10-29 12:41:54 +0100

    Initial commit

---
+++ NestedsetGrailsPlugin.groovy
@@ -0,0 +1,64 @@
+class NestedsetGrailsPlugin {
+    // the plugin version
+    def version = "0.1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.4 > *"
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    // TODO Fill in these fields
+    def title = "Nestedset Plugin"
+    def author = "Jose Gargallo"
+    def authorEmail = ""
+    def description = '''\
+The nested set model is a particular technique for representing nested sets (also known as trees or hierarchies) in relational databases. This plugin
+provides nestedset behaviour to domain classes.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/nestedset"
+
+    def license = "APACHE"
+    def organization = [ name: "Ticketbis", url: "http://engineering.ticketbis.com/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+
+    def scm = [ url: "https://github.com/ticketbis/grails-nestedset" ]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { ctx ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Nestedset
+
+[![Build
+Status](https://travis-ci.org/ticketbis/grails-nestedset.png?branch=master)](https://travis-ci.org/ticketbis/grails-nestedset)
+
+The nested set model is a particular technique for representing nested sets (also known as trees or hierarchies) in relational databases. This plugin
+provides nestedset behaviour to domain classes.
+
+## Installation
+
+Add dependency to your BuildConfig;
+
+```groovy
+compile "com.ticketbis:nestedset:0.1.0"
+```
+
+## Usage
+
+```groovy
+// grails-app/domain
+import com.ticketbis.nestedset
+
+```
+
+#### Customizing fallbacks
+
+
+#### Known issues
